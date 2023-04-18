20078149bb46764a009be5b2f6e6a82a79cffc95
belmoussaoui <emb-12@outlook.com>
2023-04-18 02:41:24 +0200

    :tada: Start 3D Flip Menu

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.querySelector('#root'))
+
+root.render(
+    <div id="scene-background">
+        <h2>3D Flip Menu</h2>
+        <h1>3D Flip Menu</h1>
+    </div>
+)
