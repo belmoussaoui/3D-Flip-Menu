8107f51b2d62fe9faf3d291ae14e9d5b2d7a290b
belmoussaoui <emb-12@outlook.com>
2023-04-19 05:19:16 +0200

    :sparkles: Add mobile frame

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+	return   <>
+	<h2>3D Flip Menu</h2>
+	<h1>3D Flip Menu</h1>
+	<img src="./frame.png" alt="" srcset="" />
+	<div className="frame"></div>
+</>
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 import './style.css'
 import { createRoot } from 'react-dom/client'
+import App from "./App.jsx"
 
 const root = createRoot(document.querySelector('#root'))
 
 root.render(
-    <div id="scene-background">
-        <h2>3D Flip Menu</h2>
-        <h1>3D Flip Menu</h1>
-    </div>
+    <App/>
 )
