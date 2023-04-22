89b304829ef623c257c121f9ccfc877215eed475
belmoussaoui <emb-12@outlook.com>
2023-04-22 22:52:41 +0200

    :sparkles: Slide Move

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,75 @@
 import { Canvas } from "@react-three/fiber"
 import Guitar from "./Components/Guitar.jsx"
 import Menu from "./Components/Menu.jsx"
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 import gsap from "gsap"
-import { Text3D, Text } from '@react-three/drei'
+import { Text3D } from '@react-three/drei'
+import Cursor from "./Components/Cursor.jsx"
 
 
 
 export default function App() {
 	const groupRef = useRef()
+	const listRef = useRef()
+	const listRef2 = useRef()
+	let test = false;
+	let side = 0;
+	let isClick = false;
+
+	const onPointerDown =(event) => {
+		isClick = true;
+	}
+
+	const onPointerUp =(event) => {
+		isClick = false;
+	}
+
+	const onPointerMove = (event) => {
+		console.log(event.movementX);
+		if (!isClick) return
+		if (!side && event.movementX > 3)
+			eventHandler();
+		if (side && event.movementX < -3)
+			eventHandler();
+		event.stopPropagation();
+	};
 
 	const eventHandler = () =>
     {
+		if (test) return
+		side = (side + 1) % 2;
+		test = true;
+		if (listRef.current.style.transform == "rotateY(0deg)")
+		{
+			listRef.current.style.transform = "rotateY(90deg)";
+			listRef2.current.style.transform = "rotateY(90deg)";
+
+		}
+		else
+		{
+			listRef.current.style.transform = "rotateY(0deg)";
+			listRef2.current.style.transform = "rotateY(0deg)";
+		}
 		gsap.to(groupRef.current.rotation, {
 			duration: 0.85,
 			y: groupRef.current.rotation.y == 0 ? Math.PI / 2 : 0,
 			ease: 'power1.inOut',
+			onComplete: () => {test = false; isClick = false;},
 		  })
     }
 	return   <>
+		<Cursor ></Cursor>
 		<span id="logo">3D Flip Menu</span>
 		<span id="background-title">3D Flip Menu</span>
-		<img src="./frame.png" alt=""/>
+		<span id="year">2023</span>
+		<span id="tuto">Click on the triple bar<br></br>or slide to left/right</span>
+		<span id="credit">Sketch by Minh Pham<br></br>Code by Bilal El Moussaoui</span>
+		<img id="phone" src="./frame.png" draggable="false"/>
 		<div className="frame">
-				<h3>Product Details</h3>
-				<Canvas flat gl={{ antialias: true }}  camera={{ fov: 60, near: 0.1, far: 200 }} onClick={eventHandler}>
+				<Canvas onPointerMove={onPointerMove} onPointerDown={onPointerDown} onPointerUp={onPointerUp} flat gl={{ antialias: true }}  camera={{ fov: 60, near: 0.1, far: 200 }}>
 				<group ref={ groupRef } rotation-y={0} position={[0,0,-1]}>
 				<directionalLight castShadow position={ [ -2, 1, 2 ] } intensity={ 2 } />
-        <ambientLight intensity={ 0.5 } />
+        		<ambientLight intensity={ 0.5 } />
 					<Menu></Menu>
 					<Text3D
 						font="Montserrat-ExtraBold.json"
@@ -35,13 +77,7 @@ export default function App() {
 						rotation-z={-Math.PI / 2}
 						size={ 0.46 }
         				height={ 0.2 }
-						// bevelEnabled
-						// bevelThickness={ 0.0001 }
-						// bevelSize={ 0.01 }
-						// bevelOffset={ 0 }
-						// bevelSegments={ 5 }
-						>
-							
+					>		
             				FENDER
 						<meshStandardMaterial color={"#aca597"} />
         			</Text3D>
@@ -61,6 +97,31 @@ export default function App() {
 					<Guitar />
 				</group>
 			</Canvas>
-		</div>
-</>
+			
+			<div id="instruments" ref={listRef}>
+				<ul>
+					<li>Guitars</li>
+					<li>Basses</li>
+					<li>Amps</li>
+					<li>Pedals</li>
+					<li>Others</li>
+				</ul>
+			</div>
+			<div id="about" ref={listRef2}>
+				<ul>
+					<li>About</li>
+					<li>Support</li>
+					<li>Terms</li>
+					<li>Faqs</li>
+				</ul>
+			</div>			
+			<div id="hamburger" onClick={eventHandler}>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+			<h3 onClick={eventHandler}>Product Details</h3>
+			</div>
+
+		</>
 }
--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,6 +1,8 @@
+import { RoundedBox } from '@react-three/drei'
+
+
 export default function Menu() {
-	return <mesh receiveShadow  scale={[3, 6.1, 2]} position-x={0} position-z={-0.25}>
-		<boxGeometry />
-		<meshBasicMaterial color="#ECE1D2"/>
+	return <mesh receiveShadow  scale={[3.1, 6.2, 2]} position-x={0} position-z={-0.25}>
+		<RoundedBox><meshBasicMaterial color="#ECE1D2"/></RoundedBox>
 	</ mesh>
 }
